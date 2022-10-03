830dd6ca6ead74f311761104d61c747675658079
Florent Huck <flovntp@gmail.com>
2022-10-03 11:23:54 +0200

    init

---
+++ admin/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
+import { HydraAdmin } from '@api-platform/admin';
+
+let entrypoint = process.env.REACT_APP_PUBLIC_URL;
+
+console.log(entrypoint);
+if(!entrypoint) { // default entrypoint to current url
+  entrypoint = window.location.href;
+}
+
+console.log(entrypoint);
+
+class App extends Component {
+  componentDidMount(){
+    document.title = process.env.REACT_APP_ADMIN_SITE_NAME || "Admin API Platform.sh";
+  }
+
+  render() {
+    return <HydraAdmin entrypoint={entrypoint}/>;
+  }
+}
+
+export default App;
