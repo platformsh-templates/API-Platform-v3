5031f170930a8ee7125282524a73c72ec1fe6404
Florent Huck <flovntp@gmail.com>
2022-10-03 14:13:53 +0200

    remove debug from api/public/index.php

--- api/public/index.php
+++ api/public/index.php
@@ -5,9 +5,5 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-
-    var_dump($context['CORS_ALLOW_ORIGIN']);
-    var_dump($context['TRUSTED_HOSTS']);
-    var_dump($context['TRUSTED_PROXIES']);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
